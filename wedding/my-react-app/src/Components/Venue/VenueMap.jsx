1a741588fce15367cf3b66e0bf7f31e846f43e21
riya-fathima-2005 <riyaf8052@gmail.com>
2026-06-23 09:53:23 +0530

    fix cors

--- wedding/my-react-app/src/Components/Venue/VenueMap.jsx
+++ wedding/my-react-app/src/Components/Venue/VenueMap.jsx
@@ -1,5 +1,18 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function VenueMap({ venues, userLocation }) {
 
